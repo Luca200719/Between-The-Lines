0466c5d7b70aeb42c90882912c399b98bc86190c
Jake-Levy14 <jzl415@psu.edu>
2026-03-29 00:20:02 -0400

    PlayerSpeakeMade

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using Unity.VisualScripting;
 
 [Serializable]
 public struct DialogueLine {
@@ -16,6 +17,11 @@ public class Dialogue : MonoBehaviour {
     TextMeshProUGUI textBoxB;
     TextMeshProUGUI bubbleBoxA;
     TextMeshProUGUI bubbleBoxB;
+    AudioSource playerSpeaker;
+
+    public AudioClip[] audioClips;
+    public AudioClip audioClipA;
+    public AudioClip audioClipB;
 
     public DialogueLine[] lines;
 
@@ -24,6 +30,11 @@ public class Dialogue : MonoBehaviour {
         textBoxB = transform.GetChild(0).GetChild(3).GetComponent<TextMeshProUGUI>();
         bubbleBoxA = transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
         bubbleBoxB = transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>();
+        playerSpeaker = GameObject.FindGameObjectWithTag("audio").GetComponent<AudioSource>();
+        audioClipA = audioClips[UnityEngine.Random.Range(0,13)];
+        audioClipB = audioClips[UnityEngine.Random.Range(0,13)];
+
+
 
         bubbleBoxA.gameObject.SetActive(false);
         bubbleBoxB.gameObject.SetActive(false);
@@ -38,14 +49,17 @@ public class Dialogue : MonoBehaviour {
     IEnumerator RunDialogue() {
         IsPlaying = true;
 
+
         foreach (var line in lines) {
             bool isA = line.speaker == DialogueLine.Speaker.A;
             string text = line.text;
 
             if (isA) {
                 textBoxB.text = string.Empty;
+                playerSpeaker.generator = audioClipA;
             } else {
                 textBoxA.text = string.Empty;
+                playerSpeaker.generator = audioClipB;
             }
 
             bubbleBoxA.gameObject.SetActive(isA);
@@ -60,13 +74,17 @@ public class Dialogue : MonoBehaviour {
             bubbleBoxText.text = text;
             bubbleBoxText.maxVisibleCharacters = 0;
 
+            playerSpeaker.Play();
+
             for (int i = 0; i <= text.Length; i++) {
                 textBoxText.maxVisibleCharacters = i;
                 bubbleBoxText.maxVisibleCharacters = i;
                 yield return new WaitForSeconds(0.05f);
             }
 
-            yield return new WaitForSeconds(1f);
+            while (playerSpeaker.isPlaying) {
+                yield return null;
+            }
         }
 
         textBoxA.text = string.Empty;
